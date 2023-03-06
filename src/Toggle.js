f636e1ffd5961cd0e190ceba5e0a2e95457a4bfc
Imaan Hooper <imaan.hooper@simpleviewinc.com>
2023-03-06 16:30:26 -0700

    10. Context With Hooks - Complete

--- src/Toggle.js
+++ src/Toggle.js
@@ -12,26 +12,3 @@ const Toggle = () => {
 }
 
 export default Toggle;
-
-// import React, { Component } from 'react';
-
-// export default class Refactor extends Component {
-//   state = {
-//     isToggled: false
-//   };
-
-//   toggle = () => {
-//     this.setState(state => {
-//       return { isToggled: !state.isToggled };
-//     });
-//   };
-
-//   render() {
-//     return (
-//       <div>
-//         <button onClick={this.toggle}>Toggle</button>
-//         {this.state.isToggled && <h2>Hello!</h2>}
-//       </div>
-//     );
-//   }
-// }
