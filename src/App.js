ebe1037009c280e4540e7e13b84827ac273196d9
Imaan Hooper <imaan.hooper@simpleviewinc.com>
2023-03-06 14:40:51 -0700

    8. Creating Custom Hooks - Complete

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import Toggle from './Toggle';
+import { useTitleInput } from './hooks/useTitleInput';
 
 const App = () => {
+  const [name, setName] = useTitleInput('');
+
   return (
     <div className="main-wrapper">
       <h1>Level Up Dishes</h1>
+      <Toggle />
+      <form onSubmit={(e) => {
+          e.preventDefault();
+        }}
+      >
+        <input 
+          type="text" 
+          onChange={(e) => setName(e.target.value)} 
+          value={name} 
+        />
+        <button>Submit</button>
+      </form>
     </div>
   );
 };
